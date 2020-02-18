e18c54543e8ce7802d031cf41d842983034dc76b
WebFaker <nicolas.seyha@gmail.com>
2020-02-18 14:58:04 +0100

    Adding updated Navbar and Footer into App

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { StyledNavbar, StyledNavbarTitle, StyledAccount } from './StyledNavbar';
+import { StyledNavbar, RedCircle, StyledAccount, MenuContainer, MenuItems, MenuItem } from './StyledNavbar';
 import { BrowserRouter as Route, Switch } from 'react-router-dom';
 
 const Navbar = () => (
     <StyledNavbar>
-      <StyledNavbarTitle>
-        PHRH
-      </StyledNavbarTitle>
-      <StyledAccount>
-        MH
-      </StyledAccount>
+      <RedCircle />
+      <MenuContainer>
+        <MenuItems>
+          <MenuItem>Gestion<br />des planificateurs</MenuItem>
+          <MenuItem>Gestion<br />des superviseurs</MenuItem>
+          <MenuItem>Gestion<br />des visiteurs</MenuItem>
+        </MenuItems>
+        <StyledAccount>
+          MH
+        </StyledAccount>
+      </MenuContainer>
     </StyledNavbar>
 );
 
