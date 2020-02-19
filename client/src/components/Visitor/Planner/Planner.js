6925c782da7af795b40e5345fcddde65bbe1a1ce
Nino Lam <Nino@MacBook-Pro-de-Nino.local>
2020-02-19 20:11:33 +0100

    add all css planner

--- client/src/components/Visitor/Planner/Planner.js
+++ client/src/components/Visitor/Planner/Planner.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Route, Link, } from 'react-router-dom';
 import './Planner.scss'
+import Tag from '../../Common/Tag/Tag'
 
 class Planner extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			visitsToPlan: [
-				{ key: 1, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 2, name: "Ibis", address: "23 avenue...", department: "92OOO", chambers: 25, lastVisit: "19/08/2019", mark: "15", urgency: "URGENCE", natureOfTheUrgency: "Chauffage" },
-				{ key: 3, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 4, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 5, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 6, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 7, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 8, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 9, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 10, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 11, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 12, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-				{ key: 13, name: "Welcomo", address: "26 avenue...", department: "93100", chambers: 26, lastVisit: "22/05/2019", mark: "29,1", urgency: "URGENCE", natureOfTheUrgency: "Moisissure" },
-
+				{ key: 1, nom: "Welcomo", adresse: "26 avenue...", cp: "93100", nb_chambres_utilise: 26, nb_visites_periode: "22/05/2019", anomalie: "29,1", urgence: "URGENCE", nature: "Moisissure" },
+				{ key: 2, nom: "Ibis", adresse: "23 avenue...", cp: "92OOO", nb_chambres_utilise: 25, nb_visites_periode: "19/08/2019", anomalie: "15", urgence: "URGENCE", nature: "Chauffage" },
+				{ key: 3, nom: "Ibis", adresse: "23 avenue...", cp: "92OOO", nb_chambres_utilise: 25, nb_visites_periode: "19/08/2019", anomalie: "15", urgence: "URGENCE", nature: "Chauffage" }
 			],
 			visitsOfTheDay: [],
 			chambersToVisit: 0
@@ -30,24 +21,24 @@ class Planner extends Component {
 
 	createVisitOfList(hotel) {
 		return (
-			<ul className="visits-to-plan" key={hotel.key} >
-				<li>{hotel.name}</li>
-				<li>{hotel.address}</li>
-				<li>{hotel.department}</li>
-				<li>{hotel.chambers}</li>
-				<li>{hotel.lastVisit}</li>
-				<li>{hotel.mark}</li>
-				<li>{hotel.urgency}</li>
-				<li>{hotel.natureOfTheUrgency}</li>
-				<span className="btnAddDelete" onClick={() => this.addToVisitOfTheDay(hotel.key)} >+</span>
-			</ul>
+			<tr key={hotel.key}>
+				<td>{hotel.nom}</td>
+				<td>{hotel.adresse}</td>
+				<td>{hotel.cp}</td>
+				<td>{hotel.nb_chambres_utilise}</td>
+				<td>{hotel.nb_visites_periode}</td>
+				<td>{hotel.anomalie}</td>
+				<td>{hotel.urgence ? <Tag name="Urgence" /> : null}</td>
+				<td>{hotel.nature}</td>
+				<td><span className="btnAddDelete" onClick={() => this.addToVisitOfTheDay(hotel.key)} >+</span></td>
+			</tr>
 		)
 	}
 
 	createVisitOfDay(hotel) {
 		return (
-			<div className="visits-of-the-day__hotel" key={hotel.key} >
-				<p>{hotel.name}</p> <span className="btnAddDelete" onClick={() => this.deleteVisitOfTheDay(hotel.key)}>-</span>
+			<div className="visits-of-the-day-hotel" key={hotel.key} >
+				<p>{hotel.nom}</p> <span className="btnAddDelete" onClick={() => this.deleteVisitOfTheDay(hotel.key)}>-</span>
 			</div>
 		)
 	}
@@ -59,7 +50,7 @@ class Planner extends Component {
 		this.setState({
 			visitsOfTheDay: this.state.visitsOfTheDay.concat(visitsToPlanClicked),
 			visitsToPlan: visitsToPlanfiltered,
-			chambersToVisit: this.state.chambersToVisit + visitsToPlanClicked.chambers
+			chambersToVisit: this.state.chambersToVisit + visitsToPlanClicked.nb_chambres_utilise
 		})
 	}
 
@@ -70,40 +61,57 @@ class Planner extends Component {
 		this.setState({
 			visitsOfTheDay: visitsOfTheDayfiltered,
 			visitsToPlan: this.state.visitsToPlan.concat(visitsOfTheDayClicked),
-			chambersToVisit: this.state.chambersToVisit - visitsOfTheDayClicked.chambers
+			chambersToVisit: this.state.chambersToVisit - visitsOfTheDayClicked.nb_chambres_utilise
+		})
+	}
+
+	clearAllVisitsOfTheDay() {
+		var visitsOfTheDay = this.state.visitsOfTheDay;
+		this.setState({
+			visitsOfTheDay: [],
+			visitsToPlan: this.state.visitsToPlan.concat(visitsOfTheDay),
+			chambersToVisit: 0
 		})
 	}
 
 	render() {
-		this.visitsToPlan = this.state.visitsToPlan.map(this.createVisitOfList)
-		this.visitsOfTheDay = this.state.visitsOfTheDay.map(this.createVisitOfDay)
+		this.visitsToPlan = this.state.visitsToPlan.map(this.createVisitOfList);
+		this.visitsOfTheDay = this.state.visitsOfTheDay.map(this.createVisitOfDay);
+		const { chambersToVisit } = this.state;
 		return (
 			<section className="planner">
 				<div className="planner-container">
 					<div className="visits-of-the-day-container">
 						<h1>Mardi 18 fevrier</h1>
+						{/* {!this.state.visitsOfTheDay.length && <p>Vous n'avez pas de visite programmer</p>} */}
 						<div className="visits-of-the-day-content">
 							<div className="visits-of-the-day">
 								{this.visitsOfTheDay}
 							</div>
-							<div>
-								<p>Vous avez <span className={this.state.chambersToVisit > 50 ? "red" : ""}> {this.state.chambersToVisit}</span> chambres à visiter</p>
-								{this.state.chambersToVisit > 50 && <p className="red">TU VAS PAS AVOIR LE TEMPS DE TOUT VISITER MON GARS</p>}
+							<p>Vous avez <span className={chambersToVisit > 50 && chambersToVisit < 60 ? "orange" : chambersToVisit >= 60 ? "red" : ""}> {chambersToVisit}</span> chambres à visiter</p>
+							{chambersToVisit > 60 && <p className="red">Vous avez atteint le nombre maximum de chambres à visiter</p>}
+							<div className="btn-container">
+								<Link to="/dashboard"><button onClick={() => this.clearAllVisitsOfTheDay()}>Annuler</button></Link>
+								<Link to="/dashboard"><button>Valider</button></Link>
 							</div>
 						</div>
 					</div>
 					<div className="visits-to-plan-container">
-						<ul className="visits-labels">
-							<li>Hôtel</li>
-							<li>Adresse</li>
-							<li>Secteur</li>
-							<li>Chambres</li>
-							<li>Date de dernière visite</li>
-							<li>Taux d’anomalie</li>
-							<li>Urgence</li>
-							<li>Nature de l’urgence</li>
-						</ul>
-						{this.visitsToPlan}
+						<table>
+							<tbody>
+								<tr className="labels">
+									<td>Hôtel</td>
+									<td>Adresse</td>
+									<td>Secteur</td>
+									<td>Chambres</td>
+									<td>Nombre de visites par mois</td>
+									<td>Taux d'anomalie</td>
+									<td>Urgence</td>
+									<td>Nature de l'urgence</td>
+								</tr>
+								{this.visitsToPlan}
+							</tbody>
+						</table>
 					</div>
 				</div>
 			</section>
