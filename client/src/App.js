3742e6545fc88c7679c46275a1226d88aec89a8e
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-02-20 18:15:04 +0100

    ⚠️  PLEASE EVERYBODY NEVER WORK DIRECTLY ON THE DEVELOP BRANCH ⚠️:

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar.js';
 import Footer from './components/Footer/Footer.js';
 import Status from './components/Visitor/Status/Status.js';
 import Dashboard from './components/Visitor/Dashboard/Dashboard.js';
-import Planner from './components/Visitor/Planner/Planner.js';
+import Planner from './components/Visitor/Dashboard/Planner/Planner.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './styles/master.scss';
 import './App.scss';
