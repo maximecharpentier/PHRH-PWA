9952edffaf59b8367136401900f67f1f9693e489
maximecharpentier <maximecharpentier0@gmail.com>
2020-02-20 20:19:10 +0100

    Add getNextWeek function in WeekSelection component

--- client/src/components/Visitor/Dashboard/WeekSelection/WeekSelection.js
+++ client/src/components/Visitor/Dashboard/WeekSelection/WeekSelection.js
@@ -22,13 +22,33 @@ class WeekSelection extends Component {
         }, () => console.log(this.state))
     }
 
+    getNextWeek = () => {
+        var currentDay = new Date();
+        currentDay.setDate(currentDay.getDate() + 7);
+        let nextWeek = []
+        
+        for (let i = 1; i <= 7; i++) {
+            let firstDayOfTheWeek = currentDay.getDate() - currentDay.getDay() + i
+            let newDay = new Date(currentDay.setDate(firstDayOfTheWeek)).toISOString().slice(0, 10)
+            nextWeek.push(newDay)
+        }
+        this.setState({
+            nextWeek
+        }, () => console.log(this.state))
+    }
+
+    componentDidMount() {
+        this.getCurrentWeek();
+        this.getNextWeek();
+    }
+
     render() { 
         
         return (
             <div className="WeekSelection">
                 <p className="WeekSelection__text">15 - 21 Fév 2020</p>
                 <div className="WeekSelection__buttons">
-                    <button className="WeekSelection__button WeekSelection__button--active" onClick={this.getCurrentWeek}>Semaine en cours</button>
+                    <button className="WeekSelection__button WeekSelection__button--active">Semaine en cours</button>
                     <button className="WeekSelection__button">Semaine suivante</button>
                 </div>
             </div>
