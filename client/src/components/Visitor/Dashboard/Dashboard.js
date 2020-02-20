341d52301ccfe8829e3598493b14c46515da30a4
maximecharpentier <maximecharpentier0@gmail.com>
2020-02-20 17:43:49 +0100

    Remove Calendar & Mode components

--- client/src/components/Visitor/Dashboard/Dashboard.js
+++ client/src/components/Visitor/Dashboard/Dashboard.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import Calendar from './Calendar/Calendar';
-import Mode from './Mode/Mode';
 import Week from './Week/Week';
 
 import './Dashboard.scss';
@@ -11,9 +9,6 @@ class Dashboard extends Component {
     render() { 
         return (
             <section className="Dashboard">
-                <div className="Dashboard__container">
-                    <Calendar />
-                </div>
                 <Week />
             </section>
         );
