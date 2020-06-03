7fd54fe5c90adbddf4745d3dac347bd4f9d3218a
pakdev1993 <pakrstic.git@gmail.com>
2020-06-04 01:31:55 +0200

    [minor] comments

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -12,48 +12,15 @@ const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
  */
 
 /**
- * 
- * @param {*} password - The plain text password
- * @param {*} hash - The hash stored in the database
- * @param {*} salt - The salt stored in the database
- * 
- * This function uses the crypto library to decrypt the hash using the salt and then compares
- * the decrypted hash/salt with the password that the user provided at login
- */
-/*function validPassword(password, hash, salt) {
-    var hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
-    return hash === hashVerify;
-}*/
-
-/**
- * 
- * @param {*} password - The password string that the user inputs to the password field in the register form
- * 
- * This function takes a plain text password and creates a salt and hash out of it.  Instead of storing the plaintext
- * password in the database, the salt and hash are stored for security
- * 
- * ALTERNATIVE: It would also be acceptable to just use a hashing algorithm to make a hash of the plain text password.
- * You would then store the hashed password in the database and then re-hash it to verify later (similar to what we do here)
- */
-/*function genPassword(password) {
-    var salt = crypto.randomBytes(32).toString('hex');
-    var genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
-    
-    return {
-      salt: salt,
-      hash: genHash
-    };
-}*/
-
-
-/**
  * @param {*} user - The user object.  We need this to set the JWT `sub` payload property to the MongoDB user ID
  */
 function issueJWT(user) {
   const _id = user._id;
 
+  //determine la durée de validité du token
   const expiresIn = '1d';
 
+  //le payload est l'objet qui fait le lien entre l'user de la BD logué et le JW Token
   const payload = {
     sub: _id,
     iat: Date.now()
