8d0b5daede814aac02de9c3ead5387fecf8b3e8e
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-06-18 17:07:04 +0200

    Fix: Lots of bugs  2

--- server/config/database.js
+++ server/config/database.js
@@ -1,45 +1,46 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const User = require("../model/user.model");
 
+const User = require("../model/user.model");
 /**
  * CONNECTION A LA BASE
  */
-const uri = `mongodb://${process.env.SERVER_HOST}:27017/PHRH`;
+
+const uri = `mongodb://localhost:27017/PHRH`;
 
 //CONNECTION (loop connect command (pour deploiement))
-var connectWithRetry = function() {
-
-    //loop connection with call back
-    return mongoose.connect(
-        uri,
-        {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-        },
-        function(err) {
-            if (err) {
-                console.error(
-                    "Failed to connect to mongo on startup - retrying in 5 sec",
-                    err
-                );
-                setTimeout(connectWithRetry, 5000);
-            }
-        }
-    );
+var connectWithRetry = function () {
+  //loop connection with call back
+  return mongoose.connect(
+    uri,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    },
+    function (err) {
+      if (err) {
+        console.error(
+          "Failed to connect to mongo on startup - retrying in 5 sec",
+          err
+        );
+        setTimeout(connectWithRetry, 5000);
+      }
+    }
+  );
 };
 connectWithRetry();
 
 //CONNECTION FAIL
-mongoose.connection.on("error", error => console.log(`Erreur de connection a l\'uri : ${uri}`, error));
+mongoose.connection.on("error", (error) =>
+  console.log(`Erreur de connection a l\'uri : ${uri}`, error)
+);
 
 //CONNECTION SUCCESS
 mongoose.connection.once("open", async () => {
+  //message
+  console.log("PHRH database connection established");
 
-    //message
-    console.log("PHRH database connection established");
-
-    //import
-    require('./import')
+  //import
+  require("./import");
 });
