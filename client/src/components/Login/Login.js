53b4ab1f64c2e90918598aff8fb298ab54d179e9
maximecharpentier <maximecharpentier0@gmail.com>
2020-07-03 21:27:05 +0200

    Add: New Login structure

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,72 +1,52 @@
 import React, { Component } from 'react'
 import styled from 'styled-components';
-import { Formik, Form, Field, ErrorMessage } from 'formik'
 import  { Redirect } from 'react-router-dom'
-//import { storeToken } from "./utils/Login.utils"
 import {API} from '../../utils/api'
 
 class Login extends Component {
-  // userLogin = (e, id) => {
-  //   e.preventDefault();
-  //   API.post('auth/login', "token").then((response) => {
-  //       console.log(response.data)
-  //       storeToken(response.data) // recupérer depuis response --> clé BEABER
-  //       // redirect to dashboard page
-  //   }).catch(error => {
-  //       console.log(error.response)
-  //   });
-  // }
-  // const oject = {
-  //   nom: null,
-  //   pwd: null
-  // }
+  constructor() {
+    super()
+    this.state = {
+      nom: '',
+      pwd: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+    console.log(this.state)
+  }
+
+  handleSubmit = () => {
+    API.post('auth/login/', this.state).then((response) => {
+      console.log(response.data)
+      localStorage.setItem('token', response.data.token);
+      localStorage.setItem('user', JSON.stringify(response.data.user));
+    }).catch(error => {
+      console.log(error.response)
+    });
+  }
 
   render() {
-    // if(){
-    //   return <Redirect />
-    // }
     return (
-      <div className="App-Login">
-      <Formik
-        initialValues={{ nom: '', pwd: '' }}
-        onSubmit={(values, { setSubmitting }) => {
-          API.post('auth/login/', values).then((response) => {
-              console.log(response.data)
-              localStorage.setItem('token', response.data.token);
-              localStorage.setItem('user', JSON.stringify(response.data.user));
-          }).catch(error => {
-              console.log(error.response)
-          });
-        }}
-        >
-          {({ isSubmitting }) => (
-            <Form className="App-Login_Form">
-              <div className="App-Login_Form_Header">
-                <h2>Connexion</h2>
-                <p>Pour continuer, connectez-vous.</p>
-              </div>
-              <div className="App-Login_Form_Field">
-                <div className="App-Login_Form_Label">
-                  <label>Nom</label>
-                </div>
-                <Field name="nom" />
-                <ErrorMessage name="nom" component="div" />
-              </div>
-              <div className="App-Login_Form_Field">
-                <div className="App-Login_Form_Label">
-                  <label>Mot de passe</label>
-                  <p className="App-Login_Form_Forgot">Mot de passe oublié ?</p>
-                </div>
-                <Field type="password" name="pwd" />
-                <ErrorMessage name="pwd" component="div" />
-              </div>
-              <button className="App-Login_Form_Button" type="submit" disabled={isSubmitting}>
-                Se connecter
-              </button>
-            </Form>
-          )}
-        </Formik>
-      </div>
+      <form>
+        <input
+          type="text"
+          value={this.state.nom}
+          name="nom"
+          placeholder="Nom d'utilisateur"
+          onChange={this.handleChange} />
+        <input
+          type="password"
+          value={this.state.pwd}
+          name="pwd"
+          placeholder="Mot de passe"
+          onChange={this.handleChange} />
+        <button type="submit" onClick={this.handleSubmit}>Valider</button>
+      </form>
     )
   }
 }
