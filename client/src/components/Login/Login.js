f8a616731a1af91dd6bea8d8f23fc83efeed8484
maximecharpentier <maximecharpentier0@gmail.com>
2020-07-05 17:52:23 +0200

    Add: Profile component style

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import { API } from '../../utils/api'
-
-import { useAuth } from "../../contexts/AuthContext";
+import { useAuth } from '../../contexts/AuthContext'
+import './Login.scss'
+import Logo from '../../assets/logo'
 
 
 const Login = () => {
@@ -30,26 +31,38 @@ const Login = () => {
     return <Redirect to="/manage" />
   }
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <input
-        type="text"
-        value={nom || ''}
-        name="nom"
-        placeholder="Nom d'utilisateur"
-        onChange={e => {
-          setNom(e.target.value);
-        }} />
-      <input
-        type="password"
-        value={pwd || ''}
-        name="pwd"
-        placeholder="Mot de passe"
-        onChange={e => {
-          setPwd(e.target.value);
-        }} />
-      <button type="submit">Valider</button>
-      {errorMsg !== "" ? <p>{errorMsg}</p> : ""}
-    </form>
+    <div className="Login">
+      <Logo />
+      <div className="Login__container">
+        <form className="Login__form" onSubmit={(e) => handleSubmit(e)}>
+          <h2 className="Login__title">Connexion</h2>
+          <p className="Login__subtitle">Pour continuer, connectez-vous.</p>
+          <label className="Login__label">Email</label>
+          <input
+            className="Login__input"
+            type="text"
+            value={nom || ''}
+            name="nom"
+            placeholder="Nom d'utilisateur"
+            onChange={e => {
+              setNom(e.target.value);
+            }}
+          />
+          <label className="Login__label">Mot de passe</label>
+          <input
+            className="Login__input"
+            type="password"
+            value={pwd || ''}
+            name="pwd"
+            placeholder="Mot de passe"
+            onChange={e => {
+              setPwd(e.target.value);
+            }} />
+          <button className="Login__button" type="submit">Se connecter</button>
+          {errorMsg !== "" ? <p className="Login__error">{errorMsg}</p> : ""}
+        </form>
+      </div>
+    </div>
   )
 }
 
