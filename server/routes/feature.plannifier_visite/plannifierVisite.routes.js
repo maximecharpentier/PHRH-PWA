296859cde08051b0ee05be9790e2a2834103d822
pakdev1993 <pakrstic.git@gmail.com>
2020-07-08 14:24:15 +0200

    [fix] route suggestion

--- server/routes/feature.plannifier_visite/plannifierVisite.routes.js
+++ server/routes/feature.plannifier_visite/plannifierVisite.routes.js
@@ -47,4 +47,33 @@ router.route('/suggestions').get(authStrategy(), async (req, res) => {
     res.status(200).json(list)
 })
 
+/**
+ * @route : recuperer la liste des visites a efectuer
+ * @method GET
+ * @param {} : objet filter (seul le secteur est dispo a l'heure acteulle) {"filters": {"secteur": X}}
+ * @return array[ (Object JSON) ]) : tableau d'object HotelRank
+ */
+router.route('/suggestions/secteur/:secteur').get(authStrategy(), async (req, res) => {
+    //QUESTION : l'affichage de la liste se fait pour un binome ? -> metre des filtrs custom -> OK
+    //notes :
+        //pour les contre-visites : elles sont automatiquement set lorsque un Hotel a une anomalie
+        //quand un Hotel est ajouté : ScoreView est recalculé (voir pour mettre une note default qui permet de reperer les nouveau hotels et les traiter différament : voir groupe pour ca)
+        //quand une visite se valide : on recalcul le score ppour l'hotel
+        //quand une anomalie est ajouté : on recalcul le score pour l'hotel
+        //quand une priorisation est ajouté : on recalcul le score pour l'hotel
+        //creer une fonction cron qui chaque jour calcul si un hotel n'a pas été visité au moins trois fois en un an et met une priorisation
+
+    const options = {}
+    if(req.params.secteur) {
+        options['secteur'] = req.params.secteur
+    }
+    
+    const rankBehaviour = new RankBehaviourV1()
+    const listHotelRankObj = new ListHotelRank(rankBehaviour, reset = false)
+
+    const list = await listHotelRankObj.get(options)
+
+    res.status(200).json(list)
+})
+
 module.exports = router;
