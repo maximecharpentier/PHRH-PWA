c471d00bf480d97a4ae58dad8020507061933d9d
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-07-09 13:07:01 +0200

    Please work well I'm tired of fixing this

--- automation/set-ip-adress.sh
+++ automation/set-ip-adress.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
-sed -i.old "s#baseURL: '.*#baseURL: 'http://52.47.86.14:3001/',#g" ../client/src/utils/api.js
-CODE=$(cat ../client/src/utils/api.js | grep "baseURL: 'http://52.47.86.14:3001/'," | wc -l)
+sed -i.old "s#baseURL: '.*#baseURL: 'http://35.180.137.52:3001/',#g" ../client/src/utils/api.js
+CODE=$(cat ../client/src/utils/api.js | grep "baseURL: 'http://35.180.137.52:3001/'," | wc -l)
 
-if [ $(cat ../client/src/utils/api.js | grep "baseURL: 'http://52.47.86.14:3001/'," | wc -l) = 1 ] ; then
+if [ $(cat ../client/src/utils/api.js | grep "baseURL: 'http://35.180.137.52:3001/'," | wc -l) = 1 ] ; then
   echo "IP adress successfully writed in ../client/src/utils/api.js" 
   return 0
  else   
